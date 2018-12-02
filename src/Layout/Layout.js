4299bbc7eb4ba931730ca56db194e8de0370788a
Raphael Blehoue <raphaelsampan@gmail.com>
2018-12-02 19:11:21 +0000

    Update Readme MD

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,18 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Header from "./Header";
-import Footer from "./Footer";
-import MiniCart from "./MiniCart";
 
-const Layout = ({ children }) => (
-  <div>
-    <Header />
-    <MiniCart />
-    <div className="cart-overlay" />
-    {children}
-    <Footer />
-  </div>
-);
+const Layout = ({ children }) => <div>{children}</div>;
 
 Layout.propTypes = {
   children: PropTypes.oneOfType([
